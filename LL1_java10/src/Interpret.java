b5127073196596b820b7b58d823b319270e85176
vervi <bokugairu@live.com>
2019-11-30 13:46:50 -0500

    t

---
+++ LL1_java10/src/Interpret.java
@@ -0,0 +1,67 @@
+import java.util.LinkedList;
+
+public class Interpret {
+    /*create a method to handle the parsing/interpretation
+
+        thoughts so far
+        with the tokenizer class we are able to tokenize the input string
+        and give back a linked list of tokens
+        we want to iterate through the linked list and
+        perform the assignments via a LL1 rdp
+
+        need a function for each one of the
+
+    */
+
+/*
+
+    public Interpreter(TokenStream input) {
+        program(input);
+    }
+
+    void program(TokenStream ts){
+        assignment();
+    }
+    void assignment(){
+//see if next token is an identifier if its not throw an error
+// if it is a valid id consume token
+// and look at the next one, match the '=' token
+// if it fails throw an error else consume and look at next token
+// call expr (it will return what it should or throw an error thats dealt with elsewhere)
+//match the ";" token, if it fails throw an error
+
+
+    }
+
+    void expr(){
+
+
+
+    }
+
+    void expr_pr(){
+
+
+
+
+    }
+
+    void term(){
+
+
+
+    }
+
+    void term_pr(){
+
+
+    }
+
+    void fact(){
+
+
+    }
+*/
+
+
+}
