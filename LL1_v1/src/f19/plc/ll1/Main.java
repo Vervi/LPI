390ab5e7236c8f2f9f5acf5164c6f80497dfdeea
vervi <bokugairu@live.com>
2019-12-14 18:34:43 -0500

    multiop test

--- LL1_v1/src/f19/plc/ll1/Main.java
+++ LL1_v1/src/f19/plc/ll1/Main.java
@@ -12,9 +12,9 @@ public class Main {
         try {
             Thread.sleep(650);
             //lexer.tokenize("x = 1 ; y = 2; z= x+y;"); //works
-            lexer.tokenize("u=3*1; v=2*u ; w=(v+1)*1+2*2;");
+            //lexer.tokenize("u=3*1; v=2*u ; w=(v+1)*1+2*2;");
            // lexer.tokenize("r =3; s= 2*(r-1);t =3*r+(s-2*r);");
-           // lexer.tokenize("s=1+2;");
+            lexer.tokenize("s=2; t=1; r= (s+-t)*---(s-t);");
             tokens=lexer.getTokens();
             tokens.add(new Lexer.Token(10, "$","eoi")); //add end of input token so parser knows when to terminate
             lexer.interpret();
