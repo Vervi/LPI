cd4cc31d384d2823b85c61c98e969a1b87f05a1e
vervi <bokugairu@live.com>
2019-12-17 21:54:28 -0500

    formatting

--- LPI/src/proj/plc/f19/Main.java
+++ LPI/src/proj/plc/f19/Main.java
@@ -1,5 +1,6 @@
 package proj.plc.f19;
 
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.Scanner;
 
@@ -9,23 +10,25 @@ public class Main {
         Lexer lexer = new Lexer();
         LinkedList<Lexer.Token> tokens;
         Parser parser =new Parser();
-        System.out.println("let's test out some assignments...");
-
-      //  System.out.println("Please enter a string of assignments to test");
-      //  Scanner sc= new Scanner(System.in);
-      //  String test=sc.nextLine();
-
+        Scanner sc= new Scanner(System.in);
         try {
-            Thread.sleep(650);
-            //lexer.tokenize("x =1; y=2; z= x+y;"); //works
-            //lexer.tokenize("u=3*1; v=2*u ; w=(v+1)*1+2*2;");
-             lexer.tokenize("r =3; s=(r-1)*2; t =3*r+(s-2*r); y=r*2+(s-3*-t);");
-             //lexer.tokenize("s=1; y=2; x=3;");
-        //    lexer.tokenize(test);
+            System.out.println("A quick note on formatting...");
+            Thread.sleep(750);
+            System.out.println("if assigning 0 to a variable...");
+            Thread.sleep(750);
+            System.out.println("please enter a space between 0 and the semicolon.");
+            Thread.sleep(750);
+            System.out.println("\n\n\n\n");
+            Thread.sleep(350);
+
+            System.out.println("Now, let's have some fun with parsing...");
+            Thread.sleep(6000);
+            System.out.println("please enter a series of assignments and hit ''return' key when done.");
+
+            String test=sc.nextLine();
+            lexer.tokenize(test);
             tokens=lexer.getTokens();
             tokens.add(new Lexer.Token(10, "$","eoi")); //add end of input token so parser knows when to terminate
-
-
             parser.parse(tokens);
         }
         catch (InterruptedException e) {
